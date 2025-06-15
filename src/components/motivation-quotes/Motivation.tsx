3eeb0f0cc6fd8790697f9f78bc87be3f8842de98
vBush95 <janole.wiebusch@t-online.de>
2025-06-15 19:27:54 +0200

    refactored quote typewriter

--- src/components/motivation-quotes/Motivation.tsx
+++ src/components/motivation-quotes/Motivation.tsx
@@ -1,61 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 import { motivationQuotes } from "./Quotes";
+import QuoteTypewriter from "./QuoteTypewriter";
 import "./Motivation.css";
 
+const getRandomQuoteIndex = () =>
+  Math.floor(Math.random() * motivationQuotes.length);
+
 const Motivation = () => {
-    const [quoteText, setQuoteText] = useState("");
-    const [authorText, setAuthorText] = useState("");
-    const [fullQuote, setFullQuote] = useState({ quote: "", author: "" });
+  const [quoteIndex, setQuoteIndex] = useState(getRandomQuoteIndex);
+  const setNextQuoteIndex = () => {
+    setQuoteIndex(getRandomQuoteIndex);
+  };
 
-
-    useEffect(() => {
-        let cancelled = false;
-
-        const typeText = async (fullText: string, setter: React.Dispatch<React.SetStateAction<string>>, optionalSetter?: React.Dispatch<React.SetStateAction<string>>) => {
-            setter("");
-            if (optionalSetter) {
-                optionalSetter("");
-            }
-            for (let i = 0; i < fullText.length; i++) {
-                if (cancelled) return;
-                setter((prev) => prev + fullText[i]);
-                await new Promise((res) => setTimeout(res, 50));
-            }
-        };
-
-        const startTyping = async () => {
-            while (!cancelled) {
-                const randomIndex = Math.floor(Math.random() * motivationQuotes.length);
-                const quote = motivationQuotes[randomIndex];
-                setFullQuote(quote);
-                await typeText('"' + quote.quote + '"', setQuoteText, setAuthorText);
-                await typeText('― ' + quote.author, setAuthorText);
-                await new Promise((res) => setTimeout(res, 15000));
-                if (cancelled) return;
-            }
-        };
-
-        startTyping();
-
-        return () => {
-            cancelled = true;
-        };
-    }, []);
-
-    return (
-        <div className="motivation-quote">
-            <blockquote>
-                <p className="motivational-quote-placeholder">"{fullQuote.quote}"</p>
-                <p>{quoteText}</p>
-                {authorText && (
-                    <footer className="motivation-author">
-                        <span className="motivational-quote-placeholder"><i>― {fullQuote.author}</i></span>
-                        <span><i>{authorText}</i></span>
-                    </footer>
-                )}
-            </blockquote>
-        </div>
-    );
+  return (
+    <QuoteTypewriter
+      key={quoteIndex}
+      quote={motivationQuotes[quoteIndex]}
+      onComplete={setNextQuoteIndex}
+    />
+  );
 };
 
 export default Motivation;
