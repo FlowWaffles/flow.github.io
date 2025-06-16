aa493572390c0448fc5b04b524e4921aca4114fa
FlowWaffles <fl0w1337@aol.de>
2025-06-16 22:42:57 +0200

    more optimize

--- src/components/cat/NyanCat.tsx
+++ src/components/cat/NyanCat.tsx
@@ -1,37 +1,16 @@
-import { useState, useEffect } from 'react';
-
 const NyanCat = () => {
-    const [following, setFollowing] = useState(false);
-    const [position, setPosition] = useState({ x: 40, y: 40 });
-
-    useEffect(() => {
-        if (!following) return;
-
-        const handleMouseMove = (e:any) => {
-            setPosition({ x: e.clientX -40, y: e.clientY -40 });
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-        return () => window.removeEventListener('mousemove', handleMouseMove);
-    }, [following]);
-
-    const handleClick = () => {
-        setFollowing((prev) => !prev);
-    };
-
     return (
         <img
             src={'../assets/nyanCat.gif'}
             alt=""
             id="cat"
-            onClick={handleClick}
             style={{
                 zIndex: 1000,
                 position: 'fixed',
-                left: position.x,
-                top: position.y,
+                left: '40px',
+                top: '40px',
                 transform: 'translate(-50%, -50%)',
-                cursor: 'pointer',
+                cursor: 'noe',
                 userSelect: 'none',
                 pointerEvents: 'auto',
                 width: '80px',
