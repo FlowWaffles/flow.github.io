3c5d3b5f949b08e48ef39abd53420b65c3d80fa0
FlowWaffles <fl0w1337@aol.de>
2025-06-17 01:22:37 +0200

    404

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -6,10 +6,9 @@ const NotFound = () => {
         <div className="not-found">
             <NyanCat />
             <h1>404</h1>
-            <p><a href="https://flow.fail">This</a> is not the page you are looking for.</p>
+            <p><a href="https://flow.fail" aria-label="Home" rel="noreferrer">This</a> is not the page you are looking for.</p>
         </div>
     )
 };
 
 export default NotFound;
-
