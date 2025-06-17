8737a04db904c52a2f8e7b1e506088d3b50ca361
FlowWaffles <fl0w1337@aol.de>
2025-06-18 00:48:00 +0200

    home

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = React.memo(() => {
     return (
         <header className="bg-gray-800 text-white p-4">
             <div className="header">
-                <h1>FLOW.FAIL</h1>
+                <h1><a href="/" aria-label="Home">FLOW.FAIL</a></h1>
                 <p>... the only page you'll <a href="https://www.twitch.tv/bobross" target="_blank">never</a> need</p>
             </div>
         </header>
