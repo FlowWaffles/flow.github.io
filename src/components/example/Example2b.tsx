37812e8aa2eea1b7aec682866a45c74e46d6fc40
fkuenzel <florian.kuenzel@payback.net>
2025-06-18 17:08:51 +0200

    fix

--- src/components/example/Example2b.tsx
+++ src/components/example/Example2b.tsx
@@ -1,42 +1,66 @@
 import ExampleContainer from './ExampleContainer.tsx';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 
-const description = "✅ Use useRef when:\n" +
-    "You want to persist a value across renders, but changing it should not trigger a re-render.\n" +
-    "\n" +
-    "Good for DOM references or tracking values (like timeouts, previous values, etc.)"
+const description = '✅ Use useRef when:\n' +
+    'You want to persist a value across renders, but changing it should not trigger a re-render.\n' +
+    '\n' +
+    'Good for DOM references or tracking values (like timeouts, previous values, etc.)'
 
 const Timer = () => {
     const count = useRef(0);
+    const [count2, setCount] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
             count.current += 1;
-            console.log("Timer:", count.current);
+            console.log('Timer:', count.current);
         }, 1000);
 
         return () => clearInterval(interval);
     }, []);
 
-    return <div>Check console</div>;
+    console.log('Timer example rendered');
+    return (
+        <>
+            <div>Check console</div>
+            <button style={{
+                padding: '1rem',
+                width: '300px',
+                margin: 'auto',
+                height: '150px',
+                backgroundColor: '#0378B9FF',
+                borderRadius: '25px'
+            }} onClick={() => setCount(count2 + 1)}>
+                Count: {count2}
+            </button>
+        </>
+    );
 };
 
 const code = 'const Timer = () => {\n' +
-    '  const count = useRef(0);\n' +
+    '    const count = useRef(0);\n' +
+    '    const [count2, setCount] = useState(0);\n' +
     '\n' +
-    '  useEffect(() => {\n' +
-    '    const interval = setInterval(() => {\n' +
-    '      count.current += 1;\n' +
-    '      console.log("Timer:", count.current);\n' +
-    '    }, 1000);\n' +
+    '    useEffect(() => {\n' +
+    '        const interval = setInterval(() => {\n' +
+    '            count.current += 1;\n' +
+    '            console.log(\'Timer:\', count.current);\n' +
+    '        }, 1000);\n' +
     '\n' +
-    '    return () => clearInterval(interval);\n' +
-    '  }, []);\n' +
+    '        return () => clearInterval(interval);\n' +
+    '    }, []);\n' +
     '\n' +
-    '  return <div>Check console</div>;\n' +
+    '    console.log(\'Timer example rendered\');\n' +
+    '    return (\n' +
+    '        <>\n' +
+    '            <div>Check console</div>\n' +
+    '            <button onClick={() => setCount(count2 + 1)}>\n' +
+    '                Count: {count2}\n' +
+    '            </button>\n' +
+    '        </>\n' +
+    '    );\n' +
     '};'
-
 
 const Example2b = () => {
     return (
