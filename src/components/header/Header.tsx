9532e14d78f30ed903d30bd916e67dcea1488ac7
Flow <fl0w1337@aol.de>
2025-08-26 23:12:40 +0200

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = React.memo(() => {
     return (
-        <header className="bg-gray-800 text-white p-4">
+        <header className="header-wrapper">
             <div className="header">
                 <h1><a href="/" aria-label="Home">FLOW.FAIL</a></h1>
                 <p>... the only page you'll <a href="https://www.twitch.tv/bobross" target="_blank">never</a> need</p>
